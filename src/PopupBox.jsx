5f9f1be5847f59096c999f908dada3728fe2f69d
ahmed <ahmed.high212@gmail.com>
2023-07-06 12:31:27 +0530

    added mobile view for popup box

--- src/PopupBox.jsx
+++ src/PopupBox.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./PopupBox.module.css";
-import voilet from './assets/voilet.svg'
-import pink from './assets/pink.svg'
-import florescent from './assets/flor.png'
-import orange from './assets/orange.svg'
-import blue from './assets/blue.svg'
-import lblue from './assets/light_blue.svg'
+import voilet from "./assets/voilet.svg";
+import pink from "./assets/pink.svg";
+import florescent from "./assets/flor.png";
+import orange from "./assets/orange.svg";
+import blue from "./assets/blue.svg";
+import lblue from "./assets/light_blue.svg";
 
-const PopupBox = ({ groupName, selectedColor, onClose, onSubmit, setGroupName, setSelectedColor, setSelectedImage, selectedImage }) => {
-
+const PopupBox = ({
+  groupName,
+  selectedColor,
+  onSubmit,
+  setGroupName,
+  setSelectedImage,
+  selectedImage,
+}) => {
   const handleCreate = () => {
     onSubmit(groupName, selectedColor, selectedImage);
   };
@@ -23,35 +29,59 @@ const PopupBox = ({ groupName, selectedColor, onClose, onSubmit, setGroupName, s
       <div className={styles.modal}>
         <h2 className={styles.modalHeading}>Create New Notes Group</h2>
         <div className={styles.lab}>
-          <label htmlFor="group-name">Group Name:</label>
+          <label htmlFor="group-name" className={styles.labelll}>Group Name:</label>
           <input
             id="group-name"
             type="text"
-            placeholder="Enter your group name"
+            placeholder="Enter your group name..."
             value={groupName}
             onChange={(e) => setGroupName(e.target.value)}
+            className={styles.input_mob}
           />
           <br />
 
-
           <div className={styles.col_wrapper}>
-            <label htmlFor="group-name" className={styles.lab_color}>Choose image:</label>
+            <label htmlFor="group-name" className={styles.lab_color}>
+              Choose colour:
+            </label>
             <div className={styles.diff_color}>
-              <img src={voilet} className={styles.col} onClick={() => handleImageClick(voilet)} />
-              <img src={pink} className={styles.col} onClick={() => handleImageClick(pink)} />
-              <img src={florescent} className={styles.col} onClick={() => handleImageClick(florescent)} />
-              <img src={orange} className={styles.col} onClick={() => handleImageClick(orange)} />
-              <img src={blue} className={styles.col} onClick={() => handleImageClick(blue)} />
-              <img src={lblue} className={styles.col} onClick={() => handleImageClick(lblue)} />
+              <img
+                src={voilet}
+                className={styles.col}
+                onClick={() => handleImageClick(voilet)}
+              />
+              <img
+                src={pink}
+                className={styles.col}
+                onClick={() => handleImageClick(pink)}
+              />
+              <img
+                src={florescent}
+                className={styles.col}
+                onClick={() => handleImageClick(florescent)}
+              />
+              <img
+                src={orange}
+                className={styles.col}
+                onClick={() => handleImageClick(orange)}
+              />
+              <img
+                src={blue}
+                className={styles.col}
+                onClick={() => handleImageClick(blue)}
+              />
+              <img
+                src={lblue}
+                className={styles.col}
+                onClick={() => handleImageClick(lblue)}
+              />
             </div>
           </div>
-
         </div>
 
         <button className={styles.createButton} onClick={handleCreate}>
           Create
         </button>
-
       </div>
     </div>
   );
